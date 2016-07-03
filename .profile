1c8363ca1a750c24e9990c6bd9cdc94cd6469ec6
Lawrence Wu <git@llwu.me>
2016-07-03 09:15:30 -0400

    Add bash prompt

--- .profile
+++ .profile
@@ -1,6 +1,36 @@
 setenv() { export "$1=$2"; }
 prepath() { export PATH=$1:$PATH; }
 postpath() { export PATH=$PATH:$1; }
+
+git_prompt()
+{
+    local exit=$?
+    git rev-parse --is-inside-work-tree >/dev/null 2>&1 || return $exit
+    local color=""
+    local body="" 
+    local commit="$(git rev-parse --short HEAD 2> /dev/null)"
+    local branch="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
+    local untracked="$(git ls-files --other --directory \
+        --exclude-standard --no-empty-directory)"
+    git diff-files --quiet --ignore-submodules 2>/dev/null
+    if [[ "$?" != 0 ]]; then
+        color='\033[01;31m'
+    else
+        color='\033[00m'
+    fi
+    if [ $branch = "HEAD" ]; then
+        body="$commit"
+    else
+        body="$branch"
+    fi
+    [[ "$untracked" ]] && body="$body U"
+    git diff-index --cached --quiet HEAD --ignore-submodules -- || body="$body S"
+    printf "%b[%s]%b " "$color" "$body" "\033[00m"
+    return $exit
+}
+
+PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w $(git_prompt)\$\[\033[00m\] '
+
 cd()
 {
   if [ $# -eq 0 ]; then
--- prompt.fish
+++ prompt.fish
@@ -1,28 +1,24 @@
 function git_prompt
     set -l current_commit (git rev-parse --short HEAD 2> /dev/null)
-    if test $status -gt 0
-        return
-    end
+    test $status -gt 0; and return
+    set -l body ""
+    set -l branch (git rev-parse --abbrev-ref HEAD 2> /dev/null)
+    set untracked (git ls-files --other --directory --exclude-standard --no-empty-directory)
     git diff-files --quiet --ignore-submodules 2>/dev/null
     if test $status = 0
         set_color normal
     else
         set_color red
     end
-    set -l branch (git rev-parse --abbrev-ref HEAD 2> /dev/null)
-    echo -n '['
     if test $branch = "HEAD"
-        echo -n $current_commit
+        set body $current_commit
     else
-        echo -n $branch
+        set body $branch
     end
-    set untracked (git ls-files --other --directory --exclude-standard --no-empty-directory)
-    test "$untracked"; and echo -n " U"
-    git diff-index --cached --quiet HEAD --ignore-submodules --
-    if test $status -gt 0
-        echo -n " S"
-    end
-    echo -n ']'
+    test "$untracked"; and set body "$body U"
+    git diff-index --cached --quiet HEAD --ignore-submodules --; or set body "$body S"
+    printf "[%s]" "$body"
+    set_color normal
 end
 
 function fish_prompt
@@ -30,7 +26,6 @@ function fish_prompt
     set_color $fish_color_cwd
     echo -n (prompt_pwd)
     git_prompt
-    set_color normal
     if not test $last_status -eq 0
         set_color $fish_color_error
     end
